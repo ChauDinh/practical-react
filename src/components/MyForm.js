d3bdaf1fe217e59cd6e00a7d38e14519a12b6ca5
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-13 18:47:57 +0700

    Optimizing Form: reduce the number of functions and replace form tag instead of div tag

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -3,52 +3,41 @@ import React, { Component } from 'react'
 export default class MyForm extends Component {
 
   state = {
-    name: 'e',
+    name: '',
+    content: "",
     rememberMe: false,
     title: 'Miss.'
   }
 
   handleChange = (e) => {
+    const isCheckbox = e.target.type === 'checkbox';
     this.setState({
-      name: e.target.value
+      [e.target.name]: isCheckbox ? e.target.checked : e.target.value
     })
   }
 
-  handleCheck = (e) => {
-    console.log("called!");
-    this.setState({
-      rememberMe: e.target.checked
-    })
-  }
-
-  handleSelect = (e) => {
-    this.setState({
-      title: e.target.value
-    })
-  }
-
-  handleSubmit = () => {
+  handleSubmit = (e) => {
+    e.preventDefault();
     console.log(this.state);
   }
 
   render() {
-    console.log(this.state.name);
     return (
-      <div>
-        <input value={this.state.name} onChange={this.handleChange} />
-        <textarea value={this.state.name} onChange={this.handleChange} />
-        <input type="checkbox" onChange={this.handleCheck}/>
+      <form onSubmit={this.handleSubmit}>
+        <input name="name" value={this.state.name} onChange={this.handleChange} />
+        <textarea name="content" value={this.state.content} onChange={this.handleChange} />
+        <input name="rememberMe" type="checkbox" onChange={this.handleChange}/>
 
         <div>
-          <select value={this.state.title} onChange={this.handleSelect}>
+          <select name="title" value={this.state.title} onChange={this.handleChange}>
             <option>Mr.</option>
             <option>Miss.</option>
             <option>Ms.</option>
             <option>Mrs.</option>
           </select>
         </div>
-        <button onClick={this.handleSubmit}>submit</button>
-      </div>
+        <button type="submit">submit</button>
+      </form>
     )
   }
 }
