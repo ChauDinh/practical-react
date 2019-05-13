a443e8e3001ee4a32f31f112553b8ac9731d9916
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-13 13:03:41 +0700

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import ImageSlider from './components/ImageSlider';
 
-function App() {
-  return (
-    <div className="App">
-      <ImageSlider />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    visible: true
+  }
+
+  render() {
+
+    const buttonText = this.state.visible ? "hide" : "show";
+
+    return (
+      <div className="App">
+        {this.state.visible ? <ImageSlider /> : <div>I'm hidden</div> }
+        <button onClick={() => {
+          this.setState({
+            visible: !this.state.visible
+          })
+        }}>{buttonText}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
