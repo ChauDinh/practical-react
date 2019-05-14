fc9693a8fc575fd92b52c580134d5a3c0b8473f6
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-14 15:10:41 +0700

    Lifting up states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './App.css';
-import FetchRandomUser from './components/FetchRandomUser';
+import Counter from "./components/Counter"
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    console.log("mounting...");
+    setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000);
+  }
+
+  handleButtonIncrement = () => {
+    let count = this.state.count;
+    this.setState({
+      count: count + 1
+    });
+  }
+
+  handleButtonDecrement = () => {
+    let count = this.state.count;
+    this.setState({
+      count: count - 1
+    });
+  }
 
   render() {
 
     return (
       <div className="App">
-        <FetchRandomUser />
+        <Counter count={this.state.count} handleButtonIncrement={this.handleButtonIncrement} handleButtonDecrement={this.handleButtonDecrement} />
       </div>
     );
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,6 @@
 import React, { Component } from 'react'
 
 export default class Counter extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: this.props.initCount
-    }
-  }
-
-  handleButtonIncrement = () => {
-    let count = this.state.count;
-    this.setState({
-      count: count + 1
-    });
-  }
-
-  handleButtonDecrement = () => {
-    let count = this.state.count;
-    this.setState({
-      count: count - 1
-    });
-  }
-
-  componentDidMount() {
-    console.log("mounting...");
-    setInterval(() => {
-      this.setState({
-        count: this.state.count + 1
-      });
-    }, 1000);
-  }
 
   componentWillUnmount() {
     console.log("unmounting...");
@@ -39,9 +9,9 @@ export default class Counter extends Component {
   render() {
     return (
       <div>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handleButtonDecrement}>decrement</button>
-        <button onClick={this.handleButtonIncrement}>increment</button>
+        <div>count: {this.props.count}</div>
+        <button onClick={this.props.handleButtonDecrement}>decrement</button>
+        <button onClick={this.props.handleButtonIncrement}>increment</button>
       </div>
     )
   }
